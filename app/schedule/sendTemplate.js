42111214a7ac99a5fb7bfb7cde7f6e3232698cb8
June <1601745371@qq.com>
2022-08-22 11:14:09 +0800

    fix: 修改定时器配置

--- app/schedule/sendTemplate.js
+++ app/schedule/sendTemplate.js
@@ -4,7 +4,7 @@ class SendTemplate extends Subscription {
     static get schedule() {
         return {
             cron: '0 0 8 1/1 * ?', // cron表达式生成器 http://cron.ciding.cc/
-            type: 'all', // 指定所有的 worker 都需要执行
+            type: 'worker', // 指定所有的 worker 都需要执行
         };
     }
 
