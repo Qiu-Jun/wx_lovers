8cb2503e256ba0e6364c6667bab331691b5db237
June <1601745371@qq.com>
2023-04-09 10:39:52 +0800

    fix: 删除服务端的schedule定时任务

--- app.js
+++ app.js
@@ -50,9 +50,6 @@ class AppBootHook {
       method: 'GET',
       dataType: 'json',
     });
-    console.log('hahh')
-    console.log(this.app.config)
-
     //   // http / https server 已启动，开始接受外部请求
     //   // 此时可以从 app.server 拿到 server 的实例
 
