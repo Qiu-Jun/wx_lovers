490ded21e1d1f3ef67ef4288e04033b48b31ebee
June <1601745371@qq.com>
2023-05-03 10:24:23 +0800

    fix: 处理第三方接口异常提醒

--- app/service/notifyUtils.js
+++ app/service/notifyUtils.js
@@ -36,66 +36,55 @@ class NotifyUtils extends Service {
 
     // 获取随机励志吉言
     async getLizhi() {
-        try {
-            const { app } = this
-            const res = await app.curl(apiUrl.words, {
-                method: 'GET',
-                dataType: 'json'
-            })
+        const { app } = this
+        const res = await app.curl(apiUrl.words, {
+            method: 'GET',
+            dataType: 'json'
+        })
+        if(res.status === 200) {
             return res.data.hitokoto || null
-        } catch (error) {
-            return null
+        } else {
+            throw new Error("随机励志吉言获取失败，这是一个免费的借口")
         }
     }
 
     // 获取天气
     async getWether() {
-        try {
-            const { app, service } = this
-            // let cityData = fs.readFileSync('./utils/usercity.json', 'utf8')
-            let cityData = app.config.userCity || null
-            if(!cityData) return null
-            const res = await app.curl(`${apiUrl.gdWether}`, {
-                method: 'GET',
-                dataType: 'json',
-                data: {
-                    key: app.config.apiConfig.amap.appKey,
-                    city: app.config.userData.city,
-                    city: cityData.adcode
-                }
-            })
-            
-            if(res.status === 200 && res.data.status === '1') {
-                const wether = res.data.lives[0]
-                return wether
-            } else {
-                return null
+        const { app, service } = this
+        // let cityData = fs.readFileSync('./utils/usercity.json', 'utf8')
+        let cityData = app.config.userCity || null
+        if(!cityData) return null
+        const res = await app.curl(`${apiUrl.gdWether}`, {
+            method: 'GET',
+            dataType: 'json',
+            data: {
+                key: app.config.apiConfig.amap.appKey,
+                city: app.config.userData.city,
+                city: cityData.adcode
             }
-            
-            
-        } catch (error) {
-            console.log(error)
-            return null
+        })
+        
+        if(res.status === 200 && res.data.status === '1') {
+            const wether = res.data.lives[0]
+            return wether
+        } else {
+            throw new Error('天气接口请求失败')
         }
+            
     }
 
     // 获取彩虹屁
     async getCaihongPi() {
-        try {
-            const { app } = this
-            const res = await app.curl(`${apiUrl.tianxing}/caihongpi/index?key=${app.config.apiConfig.tianxingKey}`, {
-                method: 'GET',
-                dataType: 'json'
-            })
-            if(res.status === 200 && res.data.code === 200) {
-                return res.data.newslist[0].content
-            } else {
-                return null
-            }
-        } catch (error) {
-            return null
+        const { app } = this
+        const res = await app.curl(`${apiUrl.tianxing}/caihongpi/index?key=${app.config.apiConfig.tianxingKey}`, {
+            method: 'GET',
+            dataType: 'json'
+        })
+        if(res.status === 200 && res.data.code === 200) {
+            return res.data.newslist[0].content
+        } else {
+            throw new Error('第三方天行彩虹屁接口请求失败')
         }
-        
     }
 
     // aiText
